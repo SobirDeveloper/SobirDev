e0ffb48582b4b76c855d604605c73071e2e856c5
SobirDeveloper <sobirovsobirjon2007@gmail.com>
2024-11-27 16:08:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Tugmalarga havola qo'shish
+document.getElementById('telegramBtn').addEventListener('click', () => {
+    window.open('https://t.me/sobirdev', '_blank');
+});
+
+document.getElementById('instagramBtn').addEventListener('click', () => {
+    window.open('https://instagram.com/sobirdev', '_blank');
+});
+
+document.getElementById('tiktokBtn').addEventListener('click', () => {
+    window.open('https://www.tiktok.com/@sobirdev', '_blank');
+});
+
+document.getElementById('youtubeBtn').addEventListener('click', () => {
+    window.open('https://www.youtube.com/channel/sobirdev', '_blank');
+});
